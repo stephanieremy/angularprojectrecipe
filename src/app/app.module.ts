3d911f95779275479b33c4598d616a42fd660eeb
stephanieremy <stephanie_remy@yahoo.fr>
2019-04-18 16:07:04 +0200

    Structure of the project Design of the header creation of a recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { ShoppinglisteditComponent } from './shoppinglist/shoppinglistedit/shoppinglistedit.component';
+import { RecipelistComponent } from './recipe/recipelist/recipelist.component';
+import { RecipeitemComponent } from './recipe/recipelist/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './recipe/recipedetail/recipedetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppinglistComponent,
+    ShoppinglisteditComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shoppinglist/shoppinglistedit/shoppinglistedit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShoppinglisteditComponent } from './shoppinglistedit.component';
+
+describe('ShoppinglisteditComponent', () => {
+  let component: ShoppinglisteditComponent;
+  let fixture: ComponentFixture<ShoppinglisteditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShoppinglisteditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShoppinglisteditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
